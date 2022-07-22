b2564689ec11e63d2309311545169b5e42a34412
Kyle Tillson <kyletillson004@gmail.com>
2022-07-22 11:13:37 -0600

    commit tree 45abda569d9ab2e945cd0c521b0c1a92096e761d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const drivers = []
+
+
+function findMatching(collection, name) {
+    return collection.filter (
+        (match) => match.toLowerCase() === name.toLowerCase()
+    )
+};
+
+
+function fuzzyMatch(collection, letters) {
+    return collection.filter (
+        (match) => match.toLowerCase().indexOf(letters.toLowerCase()) === 0
+       
+    )
+}
+
+
+function matchName(collection, name) {
+    return collection.filter (
+        ((match) => match.name === name)
+    )
+}
